4cd023cad1a691b4d96c4b51a9d9f45af01b353b
H20B <64528758+H20B@users.noreply.github.com>
2021-08-02 17:39:29 +0700

    ---

---
+++ BeatBox_Chap20/app/src/androidTest/java/com/bignerdranch/android/beatbox/SoundViewModelTest.kt
@@ -0,0 +1,22 @@
+package com.bignerdranch.android.beatbox
+
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Before
+import org.junit.Test
+
+class SoundViewModelTest {
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun exposesSoundNameAsTitle() {
+        val subject = null
+
+        assertThat(subject.title, `is`(sound.name))
+    }
+}
+
+
+
